3beb29f55752b85f3c91c7b071c86c1ea45d294f
alex <2919798881@qq.com>
2018-11-13 15:30:51 +0800

    cc

--- src/notify-translate.js
+++ src/notify-translate.js
@@ -8,33 +8,38 @@ const keys = {};
 
 gkm.events.on('key.*', data => {
   const button = data[0];
-  if (gkm.events.event === 'key.pressed' && button.indexOf('Alt')>-1 ) {
-    keys['Alt']=1;
+  console.log(button);
+  if (gkm.events.event === 'key.pressed' && button.indexOf('Control')>-1 ) {
+    keys['CTRL']=1;
   }
-  if (gkm.events.event === 'key.released' && button.indexOf('Alt')>-1 ) {
-    keys['Alt']=0;
+  if (gkm.events.event === 'key.released' && button.indexOf('Control')>-1 ) {
+    keys['CTRL']=0;
+    keys['C']=0;
   }
 
-  if (gkm.events.event === 'key.released' && keys['Alt']==1  && button ==='T') {
+  if (gkm.events.event === 'key.released' && keys['CTRL']==1  && button ==='C') {
+    keys['C']+=1;
+    if(keys['C']>=2){
+      let rawText = ncp.paste().trim();
+      if(rawText)translate(rawText, {
+          raw: true,
+          to: escape(rawText).indexOf("%u") < 0 ? "zh-CN" : "en"
+        }).then(result=>{
+          console.log(result);
+          notifier.notify(
+            {
+              title: '翻译',
+              message: result,
+              timeout: 10,
+              wait: true // Wait with callback, until user action is taken against notification
+            },
+            function(err, response) {
+              // Response is response from notification
+            }
+          );
+  
+        })
+    }
 
-    let rawText = ncp.paste().trim();
-    if(rawText)translate(rawText, {
-        raw: true,
-        to: escape(rawText).indexOf("%u") < 0 ? "zh-CN" : "en"
-      }).then(result=>{
-        console.log(result);
-        notifier.notify(
-          {
-            title: '翻译',
-            message: result,
-            timeout: 10,
-            wait: true // Wait with callback, until user action is taken against notification
-          },
-          function(err, response) {
-            // Response is response from notification
-          }
-        );
-
-      })
   }
 });
