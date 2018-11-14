d553448930fc29c83724ad450ef337ad6163b647
alex <2919798881@qq.com>
2018-11-14 16:45:30 +0800

    optimization

--- src/notify-translate.js
+++ src/notify-translate.js
@@ -5,41 +5,48 @@ var translate = require("./translate");
 
 
 const keys = {};
+keys['CTRL']=0;
+let trans = ()=>{
+  let rawText = ncp.paste().trim();
+  if(rawText)translate(rawText, {
+      raw: true,
+      to: escape(rawText).split('%u').length<rawText.length/2 ? "zh-CN" : "en"
+    }).then(result=>{
+      console.log(result);
+      notifier.notify(
+        {
+          title: '翻译',
+          message: result,
+          timeout: 10,
+          wait: true // Wait with callback, until user action is taken against notification
+        },
+        function(err, response) {
+          // Response is response from notification
+        }
+      );
 
-gkm.events.on('key.*', data => {
+    })
+};
+let listener = data => {
   const button = data[0];
-  console.log(button);
-  if (gkm.events.event === 'key.pressed' && button.indexOf('Control')>-1 ) {
-    keys['CTRL']=1;
-  }
-  if (gkm.events.event === 'key.released' && button.indexOf('Control')>-1 ) {
-    keys['CTRL']=0;
-    keys['C']=0;
+
+
+  if (gkm.events.event === 'key.pressed'  ) {
+
+    if(button.indexOf('Control')>-1)keys['CTRL']=1;
+    else if(button.indexOf('Alt')>-1)keys['CTRL']=2;
+    else if(button.indexOf('Meta')>-1)keys['CTRL']=2;
+
   }
 
-  if (gkm.events.event === 'key.released' && keys['CTRL']==1  && button ==='C') {
-    keys['C']+=1;
-    if(keys['C']>=2){
-      let rawText = ncp.paste().trim();
-      if(rawText)translate(rawText, {
-          raw: true,
-          to: escape(rawText).split('%u').length<5 ? "zh-CN" : "en"
-        }).then(result=>{
-          console.log(result);
-          notifier.notify(
-            {
-              title: '翻译',
-              message: result,
-              timeout: 10,
-              wait: true // Wait with callback, until user action is taken against notification
-            },
-            function(err, response) {
-              // Response is response from notification
-            }
-          );
-  
-        })
+  if (gkm.events.event === 'key.released' && keys['CTRL']>=1  && button ==='C') {
+    keys['CTRL'] +=1;
+    if(keys['CTRL']>2){
+      keys['CTRL'] =0;
+      trans();
     }
 
   }
-});
+};
+gkm.events.on('key.pressed',listener );
+gkm.events.on('key.released',listener );
